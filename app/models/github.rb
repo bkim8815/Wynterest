ebf2740ae50bc718a3979454a1ffbee1118a6ea9
Aldrin Bustos <multpod@yahoo.com>
2016-02-08 16:57:19 -0500

    updated display of images into grid

--- app/models/github.rb
+++ app/models/github.rb
@@ -2,17 +2,7 @@ class Github
 
   def get_profile(search)
 
-
-
-
-
-
-
-
-
-
-
-    HTTParty.get("http://api.social-searcher.com/v2/search?q=#{search}&type=photo&network=instagram,flickr&limit=100&key=f850f3cd5c87c463532bd53e061c694e")
+    HTTParty.get("http://api.social-searcher.com/v2/search?q=#{search}&type=photo&network=flickr&limit=100&key=f850f3cd5c87c463532bd53e061c694e")
 
   end
 
